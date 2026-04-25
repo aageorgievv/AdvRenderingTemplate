82fca0c663bf8035c84787a47d7fbbe0f15c8759
Aleksandar Georgiev <549190@student.saxion.nl>
2026-04-25 17:34:28 +0200

    Cleanup + GenerateBodiesClustered method

--- Codebase/RawEngine_v2_2526_3/RawEngine_v2/main.cpp
+++ Codebase/RawEngine_v2_2526_3/RawEngine_v2/main.cpp
@@ -1,13 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <fstream>
-#include <sstream>
 #include <algorithm>
-#include "core/mesh.h"
-#include "core/assimpLoader.h"
-#include "core/texture.h"
 #include <vector>
 #include <chrono>
 #include <random>
@@ -32,304 +26,253 @@
 int g_width = 1280;
 int g_height = 720;
 
-void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
 
-void framebufferSizeCallback(GLFWwindow *window,
-                             int width, int height) {
-    g_width = width;
-    g_height = height;
-    glViewport(0, 0, width, height);
-}
-
-std::string readFileToString(const std::string &filePath) {
-    std::ifstream fileStream(filePath, std::ios::in);
-    if (!fileStream.is_open()) {
-        printf("Could not open file: %s\n", filePath.c_str());
-        return "";
-    }
-    std::stringstream buffer;
-    buffer << fileStream.rdbuf();
-    return buffer.str();
-}
-
-GLuint generateShader(const std::string &shaderPath, GLuint shaderType) {
-    printf("Loading shader: %s\n", shaderPath.c_str());
-    const std::string shaderText = readFileToString(shaderPath);
-    const GLuint shader = glCreateShader(shaderType);
-    const char *s_str = shaderText.c_str();
-    glShaderSource(shader, 1, &s_str, nullptr);
-    glCompileShader(shader);
-    GLint success = 0;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        printf("Error! Shader issue [%s]: %s\n", shaderPath.c_str(), infoLog);
-    }
-    return shader;
+void framebufferSizeCallback(GLFWwindow* window,
+	int width, int height) {
+	g_width = width;
+	g_height = height;
+	glViewport(0, 0, width, height);
 }
 
 //My code -------------------------------------------------------------------------------------------
 
 struct Body2D {
-    glm::vec2 position;
-    glm::vec2 halfSize;
+	glm::vec2 position;
+	glm::vec2 halfSize;
 };
 
 struct CollisionStats {
-    int candidatePairs = 0;
-    int collisionPairs = 0;
+	int candidatePairs = 0;
+	int collisionPairs = 0;
 };
 
 
 bool Overlaps(const Body2D& a, const Body2D& b) {
-    float aMinX = a.position.x - a.halfSize.x;
-    float aMaxX = a.position.x + a.halfSize.x;
-    float aMinY = a.position.y - a.halfSize.y;
-    float aMaxY = a.position.y + a.halfSize.y;
+	float aMinX = a.position.x - a.halfSize.x;
+	float aMaxX = a.position.x + a.halfSize.x;
+	float aMinY = a.position.y - a.halfSize.y;
+	float aMaxY = a.position.y + a.halfSize.y;
 
-    float bMinX = b.position.x - b.halfSize.x;
-    float bMaxX = b.position.x + b.halfSize.x;
-    float bMinY = b.position.y - b.halfSize.y;
-    float bMaxY = b.position.y + b.halfSize.y;
+	float bMinX = b.position.x - b.halfSize.x;
+	float bMaxX = b.position.x + b.halfSize.x;
+	float bMinY = b.position.y - b.halfSize.y;
+	float bMaxY = b.position.y + b.halfSize.y;
 
-    return aMinX <= bMaxX && aMaxX >= bMinX &&
-        aMinY <= bMaxY && aMaxY >= bMinY;
+	return aMinX <= bMaxX && aMaxX >= bMinX &&
+		aMinY <= bMaxY && aMaxY >= bMinY;
 }
 
 CollisionStats RunBruteForce(const std::vector<Body2D>& bodies) {
 
-    CollisionStats stats;
+	CollisionStats stats;
 
-    for (int i = 0; i < (int)bodies.size(); ++i) {
-        for (int j = i + 1; j < bodies.size(); ++j) {
-            stats.candidatePairs++;
-            if (Overlaps(bodies[i], bodies[j])) {
-                stats.collisionPairs++;
-            }
-        }
-    }
+	for (int i = 0; i < (int)bodies.size(); ++i) 
+	{
+		for (int j = i + 1; j < bodies.size(); ++j) 
+		{
+			stats.candidatePairs++;
+			if (Overlaps(bodies[i], bodies[j])) {
+				stats.collisionPairs++;
+			}
+		}
+	}
 
-    return stats;
+	return stats;
 }
 
 std::vector<Body2D> GenerateBodiesGrid(int count) {
 
-    std::vector<Body2D> bodies;
-    bodies.reserve(count);
+	std::vector<Body2D> bodies;
+	bodies.reserve(count);
 
-    for (int i = 0; i < count; i++) {
-        float x = -5.0f + (i % 20) * 0.5f;
-        float y = -5.0f + (i / 20) * 0.5f;
+	for (int i = 0; i < count; i++) 
+	{
+		float x = -5.0f + (i % 20) * 0.5f;
+		float y = -5.0f + (i / 20) * 0.5f;
 
-        Body2D body;
-        body.position = glm::vec2(x, y);
-        body.halfSize = glm::vec2(0.2f, 0.2f);
+		Body2D body;
+		body.position = glm::vec2(x, y);
+		body.halfSize = glm::vec2(0.2f, 0.2f);
 
-        bodies.push_back(body);
-    }
+		bodies.push_back(body);
+	}
 
-    return bodies;
+	return bodies;
 }
 
 std::vector<Body2D> GenerateBodiesRandom(int count, unsigned int seed) {
-    std::vector<Body2D> bodies;
-    bodies.reserve(count);
+	std::vector<Body2D> bodies;
+	bodies.reserve(count);
 
-    std::mt19937 rng(seed);
-    std::uniform_real_distribution<float> xDist(-5.0f, 5.0f);
-    std::uniform_real_distribution<float> yDist(-5.0f, 5.0f);
+	std::mt19937 rng(seed);
+	std::uniform_real_distribution<float> xDist(-5.0f, 5.0f);
+	std::uniform_real_distribution<float> yDist(-5.0f, 5.0f);
 
-    for (int i = 0; i < count; i++)
-    {
-        Body2D body;
-        body.position = glm::vec2(xDist(rng), yDist(rng));
-        body.halfSize = glm::vec2(0.2f, 0.2f);
-        bodies.push_back(body);
-    }
+	for (int i = 0; i < count; i++)
+	{
+		Body2D body;
+		body.position = glm::vec2(xDist(rng), yDist(rng));
+		body.halfSize = glm::vec2(0.2f, 0.2f);
+		bodies.push_back(body);
+	}
 
-    return bodies;
+	return bodies;
+}
+
+std::vector<Body2D> GenerateBodiesClustered(int count, unsigned int seed) {
+	std::vector<Body2D> bodies;
+	bodies.reserve(count);
+
+	std::mt19937 rng(seed);
+	std::uniform_real_distribution<float> centerDist(-4.0f, 4.0f);
+	std::uniform_real_distribution<float> offsetDist(-0.8f, 0.8f);
+
+	const int clusterCount = 4;
+	std::vector<glm::vec2> centers;
+	centers.reserve(clusterCount);
+
+	for (int i = 0; i < clusterCount; ++i) 
+	{
+		centers.push_back(glm::vec2(centerDist(rng), centerDist(rng)));
+	}
+
+	for (int i = 0; i < count; ++i) 
+	{
+		int clusterIndex = i % clusterCount;
+
+		Body2D body;
+		body.position = centers[clusterIndex] + glm::vec2(offsetDist(rng), offsetDist(rng));
+		body.halfSize = glm::vec2(0.2f, 0.2f);
+
+		bodies.push_back(body);
+	}
+	return bodies;
 }
 
 
 int main() {
-    glfwInit(); 
-    glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwInit();
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow *window = glfwCreateWindow(g_width, g_height, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(g_width, g_height, "LearnOpenGL", NULL, NULL);
 
-    if (window == NULL) {
-        printf("Failed to create GLFW window\n");
-        glfwTerminate();
-        return -1;
-    }
+	if (window == NULL) {
+		printf("Failed to create GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
 
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 
-    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-        printf("Failed to initialize GLAD\n");
-        return -1;
-    }
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		printf("Failed to initialize GLAD\n");
+		return -1;
+	}
 
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
-    //Setup platforms
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 400");
+	//Setup platforms
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 400");
 
-    glEnable(GL_DEPTH_TEST);
-    glFrontFace(GL_CCW);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+	glFrontFace(GL_CCW);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    const GLuint modelVertexShader = generateShader("shaders/modelVertex.vs", GL_VERTEX_SHADER);
-    const GLuint fragmentShader = generateShader("shaders/fragment.fs", GL_FRAGMENT_SHADER);
-    const GLuint textureShader = generateShader("shaders/texture.fs", GL_FRAGMENT_SHADER);
+	glm::vec4 clearColor = glm::vec4(0.2f, 0.2f, 0.2f, 1.0f);
+	glClearColor(clearColor.r,
+		clearColor.g, clearColor.b, clearColor.a);
 
-    int success;
-    char infoLog[512];
-    const unsigned int modelShaderProgram = glCreateProgram();
-    glAttachShader(modelShaderProgram, modelVertexShader);
-    glAttachShader(modelShaderProgram, fragmentShader);
-    glLinkProgram(modelShaderProgram);
-    glGetProgramiv(modelShaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(modelShaderProgram, 512, NULL, infoLog);
-        printf("Error! Making Shader Program: %s\n", infoLog);
-    }
-    const unsigned int textureShaderProgram = glCreateProgram();
-    glAttachShader(textureShaderProgram, modelVertexShader);
-    glAttachShader(textureShaderProgram, textureShader);
-    glLinkProgram(textureShaderProgram);
-    glGetProgramiv(textureShaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(textureShaderProgram, 512, NULL, infoLog);
-        printf("Error! Making Shader Program: %s\n", infoLog);
-    }
+	//-------------------------------------
 
-    glDeleteShader(modelVertexShader);
-    glDeleteShader(fragmentShader);
-    glDeleteShader(textureShader);
+	int distributionMode = 0; // 0 = grid , 1 = random
+	unsigned int seed = 44;
+	int bodyCount = 100;
 
-    core::Mesh quad = core::Mesh::generateQuad();
-    core::Model quadModel({quad});
+	std::vector<Body2D> bodies = GenerateBodiesGrid(bodyCount);
 
-    core::Model suzanne = core::AssimpLoader::loadModel("models/nonormalmonkey.obj");
-    core::Texture cmgtGatoTexture("textures/CMGaTo_crop.png");
+	while (!glfwWindowShouldClose(window)) {
 
-    glm::vec4 clearColor = glm::vec4(0.2f, 0.2f, 0.2f, 1.0f);
-    glClearColor(clearColor.r,
-                 clearColor.g, clearColor.b, clearColor.a);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 10.0f);
-    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
 
-    //VP
-    glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, cameraUp);
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(g_width) / static_cast<float>(g_height), 0.1f, 100.0f);
+		ImGui::NewFrame();
 
-    GLint mvpMatrixUniform = glGetUniformLocation(modelShaderProgram, "mvpMatrix");
-    GLint textureModelUniform = glGetUniformLocation(textureShaderProgram, "mvpMatrix");
-    GLint textureUniform = glGetUniformLocation(textureShaderProgram, "text");
+		auto start = std::chrono::high_resolution_clock::now();
+		CollisionStats stats = RunBruteForce(bodies);
+		auto end = std::chrono::high_resolution_clock::now();
+		double bruteForceMs = std::chrono::duration<double, std::milli >(end - start).count();
 
-    double currentTime = glfwGetTime();
-    double finishFrameTime = 0.0;
-    float deltaTime = 0.0f;
-    float rotationStrength = 100.0f;
+		ImGui::Begin("Raw Engine v2");
+		ImGui::Text("Collision Test");
+		const char* distributionItems[] = { "Grid", "Random", "Clustered"};
+		ImGui::Combo("Distribution", &distributionMode, distributionItems, 3);
+		ImGui::InputInt("Random Seed", (int*)&seed);
+		ImGui::SliderInt("Body Count", &bodyCount, 1, 5000);
 
-    //-------------------------------------
+		if (ImGui::Button("Regenerate Bodies")) {
+			if (distributionMode == 0) {
+				bodies = GenerateBodiesGrid(bodyCount);
+			}
+			else if (distributionMode == 1) {
+				bodies = GenerateBodiesRandom(bodyCount, seed);
+			}
+			else if (distributionMode == 2) {
+				bodies = GenerateBodiesClustered(bodyCount, seed);
+			}
+		}
 
-    int distributionMode = 0; // 0 = grid , 1 = random
-    unsigned int seed = 44;
-    int bodyCount = 100;
+		ImGui::Text("Candidate pairs: %d", stats.candidatePairs);
+		ImGui::Text("Collision pairs: %d", stats.collisionPairs);
+		ImGui::Separator();
+		ImGui::Text("Brute force time: %.4f ms", bruteForceMs);
+		ImGui::Separator();
 
-    std::vector<Body2D> bodies = GenerateBodiesGrid(bodyCount);
+		int previewCount = std::min((int)bodies.size(), 5);
+		for (int i = 0; i < previewCount; ++i) {
+			ImGui::Text("Body %d: pos(%.2f, %.2f) halfSize(%.2f, %.2f)",
+				i,
+				bodies[i].position.x,
+				bodies[i].position.y,
+				bodies[i].halfSize.x,
+				bodies[i].halfSize.y);
+		}
 
-    while (!glfwWindowShouldClose(window)) {
+		ImGui::End();
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		processInput(window);
 
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        ImGui::NewFrame();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
-        auto start = std::chrono::high_resolution_clock::now();
-        CollisionStats stats = RunBruteForce(bodies);
-        auto end = std::chrono::high_resolution_clock::now();
-        double bruteForceMs = std::chrono::duration<double, std::milli > (end - start).count();
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
-        ImGui::Begin("Raw Engine v2");
-        ImGui::Text("Collision Test");
-        const char* distributionItems[] = { "Grid", "Random" };
-        ImGui::Combo("Distribution", &distributionMode, distributionItems, 2);
-        ImGui::InputInt("Random Seed", (int*)&seed);
-        ImGui::SliderInt("Body Count", &bodyCount, 1, 5000);
-
-        if (ImGui::Button("Regenerate Bodies")) {
-            if (distributionMode == 0) {
-                bodies = GenerateBodiesGrid(bodyCount);
-            }
-            else if (distributionMode == 1) {
-                bodies = GenerateBodiesRandom(bodyCount, seed);
-            }
-        }
-
-        ImGui::Text("Candidate pairs: %d", stats.candidatePairs);
-        ImGui::Text("Collision pairs: %d", stats.collisionPairs);
-        ImGui::Separator();
-        ImGui::Text("Brute force time: %.4f ms", bruteForceMs);
-        ImGui::Separator();
-
-        int previewCount = std::min((int)bodies.size(), 5);
-        for (int i = 0; i < previewCount; ++i) {
-            ImGui::Text("Body %d: pos(%.2f, %.2f) halfSize(%.2f, %.2f)",
-                i,
-                bodies[i].position.x,
-                bodies[i].position.y,
-                bodies[i].halfSize.x,
-                bodies[i].halfSize.y);
-        }
-
-        ImGui::End();
-
-        processInput(window);
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-        finishFrameTime = glfwGetTime();
-        deltaTime = static_cast<float>(finishFrameTime - currentTime);
-        currentTime = finishFrameTime;
-    }
-
-    glDeleteProgram(modelShaderProgram);
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
